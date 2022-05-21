5e7f23427aef411aa655b713a58caffe18676705
glowcouch <42631203+glowcouch@users.noreply.github.com>
2022-05-22 11:19:02 +1200

    Create a screen and start it's mainloop

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program {
+    public static void Main (string[] args) {
+        //make a new screen and start it's mainloop
+        Screen screen = new Screen();
+        screen.StartMainLoop();
+    }
+}
